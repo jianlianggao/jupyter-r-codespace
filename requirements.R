f80282a4fdda57b59c536170eeddc18b75547fc2
Jianliang <j.gao@imperial.ac.uk>
2025-01-14 16:17:20 +0000

    Update requirements.R

--- requirements.R
+++ requirements.R
@@ -1,5 +1,17 @@
-install.packages('languageserver', ask = FALSE)
-install.packages("rmarkdown", ask = FALSE)
-install.packages('jsonlite', ask = FALSE)
-install.packages('dplyr', ask = FALSE)
+if (!require(languageserver)) {
+  install.packages("languageserver", repos="http://cran.r-project.org", ask = FALSE)
+}
+
+if (!require(rmarkdown)) {
+  install.packages("rmarkdown", repos="http://cran.r-project.org", ask = FALSE)
+}
+
+if (!require(jsonlite)) {
+  install.packages("jsonlite", repos="http://cran.r-project.org", ask = FALSE)
+}
+
+if (!require(dplyr)) {
+  install.packages("dplyr", repos="http://cran.r-project.org", ask = FALSE)
+}
+
 tinytex::install_tinytex()
